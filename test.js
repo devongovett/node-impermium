7f8e9508cd0a2cb4a7bc8209929fadd599416174
Devon Govett <devongovett@gmail.com>
2013-11-05 12:41:55 -0800

    Add test for http request option

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var impermium = require('./')
-  , assert = require('assert');
+  , assert = require('assert')
+  , http = require('http')
+  , superagent = require('superagent');
 
+impermium.API_ROOT = 'http://api-test.impermium.com/4.0/';
 impermium.API_KEY = 'YOUR_API_KEY_HERE';
 
 impermium.post({
@@ -18,8 +21,8 @@ impermium.post({
 
 impermium.post({
   post_id: '123456',
-  user_id: 'Kethreen007',
-  content: 'This story is great. I love Storify.',
+  user_id: 'someuser',
+  content: 'Great pictures!',
   post_type: 'comment',
   operation: 'create',
   timestamp: '20110424191612Z'
@@ -27,4 +30,26 @@ impermium.post({
   assert.equal(err, null);
   assert.equal(res.action, 'allow');
   assert.deepEqual(res.tags, []);
+});
+
+var server = http.createServer(function(req, res) {
+  impermium.post({
+    req: req,
+    post_id: '123456',
+    user_id: 'Kethreen007',
+    content: 'I got a 9 month old Lexus IS 350 C Convertible by work part-time using a laptop. Visit Your URL........ >. BAY89.CoM',
+    post_type: 'comment',
+    operation: 'create'
+  }, function(err, result) {
+    res.writeHead(err ? 500 : 200);
+    res.end();
+  });
+}).listen(0, function() {
+  var port = server.address().port;
+  superagent
+    .get('http://127.0.0.1:' + port)
+    .end(function(err, res) {
+      assert.equal(res.status, 200);
+      server.close();
+    });
 });
